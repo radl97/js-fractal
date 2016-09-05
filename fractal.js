efafb05b537b0202b1d99a3d0a6b4b2e13880a4b
Adrian Robotka <robotka.adrian@gmail.com>
2016-09-05 21:29:55 +0200

    reformat sources

--- fractal.js
+++ fractal.js
@@ -3,38 +3,38 @@ var svg = document.querySelector('#svg');
 
 // kitorli az SVG vonalak
 function clear() {
-  while (svg.firstChild) {
-    svg.removeChild(svg.firstChild);
-  }
+    while (svg.firstChild) {
+        svg.removeChild(svg.firstChild);
+    }
 }
 
 // egy vonalat csinal
-function createRelativePath(x1,y1,x2,y2) {
-  var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  path.setAttribute("d", "M " + x1 + " " + y1 + " l " + x2 + " " + y2);
-  path.style.stroke="#F00";
-  path.style.strokeWidth="1px";
+function createRelativePath(x1, y1, x2, y2) {
+    var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute("d", "M " + x1 + " " + y1 + " l " + x2 + " " + y2);
+    path.style.stroke = "#F00";
+    path.style.strokeWidth = "1px";
 
-  svg.appendChild(path);
+    svg.appendChild(path);
 }
 
 // egy vonalat csinal
-function createPath(x1,y1,x2,y2) {
-  var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  path.setAttribute("d", "M " + x1 + " " + y1 + " L " + x2 + " " + y2);
-  path.style.stroke="#F00";
-  path.style.strokeWidth="1px";
+function createPath(x1, y1, x2, y2) {
+    var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute("d", "M " + x1 + " " + y1 + " L " + x2 + " " + y2);
+    path.style.stroke = "#F00";
+    path.style.strokeWidth = "1px";
 
-  svg.appendChild(path);
+    svg.appendChild(path);
 }
 
 // Koch-gorbe leirasa
 var koch_spec = [ // (0px,0px); 1px, 0 rad) -> ??
 // x              y    l  r
-[  0,             0, 1/3, 0],
-[1/3,             0, 1/3, Math.PI/3],
-[1/2,Math.sqrt(3)/6, 1/3, -Math.PI/3],
-[2/3,             0, 1/3, 0],
+    [0, 0, 1 / 3, 0],
+    [1 / 3, 0, 1 / 3, Math.PI / 3],
+    [1 / 2, Math.sqrt(3) / 6, 1 / 3, -Math.PI / 3],
+    [2 / 3, 0, 1 / 3, 0],
 ];
 
 // specifikal egy fraktalt
@@ -45,26 +45,26 @@ var spec = koch_spec;
 // a vonalakat kirajzolja rekurzivan
 // x,y,l,r a fenti negyes leirasaval egyezik
 function draw(d, x, y, l, r) {
-  var c = Math.cos(r);
-  var s = Math.sin(r);
-  if (d === 0) {
-    var x1 = x;
-    var y1 = y;
-    var x2 = c*l;
-    var y2 = s*l;
-    createRelativePath(x1,y1,x2,y2);
-    return;
-  }
-  for (var i=0; i < spec.length; i++) {
-    var sp = spec[i];
-    var x2 = sp[0], y2 = sp[1], l2 = sp[2], r2 = sp[3];
-    // (x;y) -> (x*c+y*s; -x*s+y*x)
-    var x3 = l*(x2*c-y2*s) + x;
-    var y3 = l*(y2*c+x2*s) + y;
-    var l3 = l2*l;
-    var r3 = r2 + r;
-    draw(d-1, x3, y3, l3, r3);
-  }
+    var c = Math.cos(r);
+    var s = Math.sin(r);
+    if (d === 0) {
+        var x1 = x;
+        var y1 = y;
+        var x2 = c * l;
+        var y2 = s * l;
+        createRelativePath(x1, y1, x2, y2);
+        return;
+    }
+    for (var i = 0; i < spec.length; i++) {
+        var sp = spec[i];
+        var x2 = sp[0], y2 = sp[1], l2 = sp[2], r2 = sp[3];
+        // (x;y) -> (x*c+y*s; -x*s+y*x)
+        var x3 = l * (x2 * c - y2 * s) + x;
+        var y3 = l * (y2 * c + x2 * s) + y;
+        var l3 = l2 * l;
+        var r3 = r2 + r;
+        draw(d - 1, x3, y3, l3, r3);
+    }
 }
 
 clear();
