a44e66282fab363961fdccec2200780235c70519
Adrian Robotka <robotka.adrian@gmail.com>
2016-09-06 22:17:02 +0200

    start redesign js logic

--- js/fractal.js
+++ js/fractal.js
@@ -1,76 +1,99 @@
-// megkeressuk az SVG DOM-unkat
-var svg = document.querySelector('#svg');
-
-// kitorli az SVG vonalak
-function clear() {
-    while (svg.firstChild) {
-        svg.removeChild(svg.firstChild);
+/**
+ * Clears the draw table
+ * @param drawTable The draw table SVG DOM element
+ */
+function clearDrawTable(drawTable) {
+    while (drawTable.firstChild) {
+        drawTable.removeChild(drawTable.firstChild);
     }
 }
 
-// egy vonalat csinal
-function createRelativePath(x1, y1, x2, y2) {
+/**
+ * Draw a line to the draw table
+ * @param drawTable The draw table SVG DOM element
+ * @param x1 X coordinate of the start point
+ * @param y1 Y coordinate of the start point
+ * @param x2 X coordinate of the end point
+ * @param y2 Y coordinate of the end point
+ */
+function drawLine(drawTable, x1, y1, x2, y2) {
     var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     path.setAttribute("d", "M " + x1 + " " + y1 + " l " + x2 + " " + y2);
     path.style.stroke = "#F00";
     path.style.strokeWidth = "1px";
 
-    svg.appendChild(path);
+    drawTable.appendChild(path);
 }
 
-// egy vonalat csinal
-function createPath(x1, y1, x2, y2) {
-    var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.setAttribute("d", "M " + x1 + " " + y1 + " L " + x2 + " " + y2);
-    path.style.stroke = "#F00";
-    path.style.strokeWidth = "1px";
-
-    svg.appendChild(path);
-}
-
-// Koch-gorbe leirasa
+/**
+ * Koch-curve description
+ * @type {*[]}
+ */
 var koch_spec = [ // (0px,0px); 1px, 0 rad) -> ??
 // x              y    l  r
     [0, 0, 1 / 3, 0],
     [1 / 3, 0, 1 / 3, Math.PI / 3],
     [1 / 2, Math.sqrt(3) / 6, 1 / 3, -Math.PI / 3],
-    [2 / 3, 0, 1 / 3, 0],
+    [2 / 3, 0, 1 / 3, 0]
 ];
 
-// specifikal egy fraktalt
 // [x,y,l,r] elemeket tartalmaz
 // (x;y)-tol +x tengelytol -r radian szogben l hosszu vonal
-var spec = koch_spec;
 
-// a vonalakat kirajzolja rekurzivan
+/**
+ * Specify a fractal
+ * TODO comment it right
+ * @type {*[]}
+ */
+var fractalSpec = koch_spec;
+
 // x,y,l,r a fenti negyes leirasaval egyezik
-function draw(d, x, y, l, r) {
+/**
+ * Draw the fractal recursively to the draw table
+ * TODO comment it right
+ * @param drawTable The draw table SVG DOM element
+ * @param d
+ * @param x
+ * @param y
+ * @param l
+ * @param r
+ */
+function draw(drawTable, d, x, y, l, r) {
     var c = Math.cos(r);
     var s = Math.sin(r);
+
     if (d === 0) {
         var x1 = x;
         var y1 = y;
         var x2 = c * l;
         var y2 = s * l;
-        createRelativePath(x1, y1, x2, y2);
+        drawLine(drawTable, x1, y1, x2, y2);
         return;
     }
-    for (var i = 0; i < spec.length; i++) {
-        var sp = spec[i];
+
+    for (var i = 0; i < fractalSpec.length; i++) {
+        var sp = fractalSpec[i];
         var x2 = sp[0], y2 = sp[1], l2 = sp[2], r2 = sp[3];
         // (x;y) -> (x*c+y*s; -x*s+y*x)
         var x3 = l * (x2 * c - y2 * s) + x;
         var y3 = l * (y2 * c + x2 * s) + y;
         var l3 = l2 * l;
         var r3 = r2 + r;
-        draw(d - 1, x3, y3, l3, r3);
+        draw(drawTable, d - 1, x3, y3, l3, r3);
     }
 }
 
-clear();
-// 5 szintu gorbe (20;20) ponttol, 729 hosszu vonal helyett
-draw(5, 20, 20, 729, 0);
+/**
+ * Drow the fractal to the draw table
+ * @param drawTable The draw table SVG DOM element
+ */
+function drawFractal(drawTable) {
+    drawTable = document.querySelector(selector);
 
-console.log(svg);
+    clearDrawTable(drawTable);
+    // TODO connect this function with the GUI
+    // 5 szintu gorbe (20;20) ponttol, 729 hosszu vonal helyett
+    draw(drawTable, 5, 20, 20, 729, 0);
+}
 
-// TODO: kene egy UI, amit be lehet kapcsolni
+
