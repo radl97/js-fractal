2f638fab9b3dd8e283e13fa44f5180e05739bf50
radlaci97 <radlaci97@gmail.com>
2016-09-08 22:07:59 +0200

    Add .gitignore and rework fractal.js
    Rename variables for clarity.
    Add/finish comments for functions.
    Remove unnecessary comments.
    Fix typos.

--- js/fractal.js
+++ js/fractal.js
@@ -11,14 +11,14 @@ function clearDrawTable(drawTable) {
 /**
  * Draw a line to the draw table
  * @param drawTable The draw table SVG DOM element
- * @param x1 X coordinate of the start point
- * @param y1 Y coordinate of the start point
- * @param x2 X coordinate of the end point
- * @param y2 Y coordinate of the end point
+ * @param x X coordinate of the start point
+ * @param y Y coordinate of the start point
+ * @param dx X coordinate of the end point relative to the start point (real coordinate is x+dx)
+ * @param dy Y coordinate of the end point relative to the start point (real coordinate is y+dy)
  */
-function drawLine(drawTable, x1, y1, x2, y2) {
+function drawLine(drawTable, x, y, dx, dy) {
     var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.setAttribute("d", "M " + x1 + " " + y1 + " l " + x2 + " " + y2);
+    path.setAttribute("d", "M " + x + " " + y + " l " + dx + " " + dy);
     path.style.stroke = "#F00";
     path.style.strokeWidth = "1px";
 
@@ -26,75 +26,66 @@ function drawLine(drawTable, x1, y1, x2, y2) {
 }
 
 /**
- * Koch-curve description
- * 
- * syntax: [x,y,l,r]
- * Draw from (x,y) in r radian angle l lenght
- * 
- * @type {*[]}
+ * Draw the fractal recursively to the draw table
+ * @param drawTable The draw table SVG DOM element
+ * @param spec The specification of the fractal
+ * Syntax: An array of [x,y,length,angle] values
+ * Each element describes a line: length length from (x,y) in angle radian
+ * The specification tells that from a line between (0;0) and (1;0) we must create these lines
+ * @param depth Depth of fractal recursion
+ * @param x The X coordinate of the base line
+ * @param y The Y coordinate of the base line
+ * @param length The length of the base line
+ * @param angle The angle of the base line
  */
-var koch_spec = [ // (0px,0px); 1px, 0 rad) -> ??
-// x              y    l  r
+function draw(drawTable, spec, depth, x, y, length, angle) {
+    var cos = Math.cos(angle);
+    var sin = Math.sin(angle);
+
+    // 0'th depth means that we simply draw the line
+    if (depth === 0) {
+        var dx = cos * length;
+        var dy = sin * length;
+        drawLine(drawTable, x, y, dx, dy);
+        return;
+    }
+
+    // if the depth is non-zero, we go through all the lines specified by the fractal, and recurse there
+    for (var i = 0; i < spec.length; i++) {
+        var lineSpec = spec[i];
+        var fractalX = lineSpec[0], fractalY = lineSpec[1], fractalLength = lineSpec[2], fractalAngle = lineSpec[3];
+        var newX = length * (fractalX * cos - fractalY * sin) + x;
+        var newY = length * (fractalY * cos + fractalX * sin) + y;
+        var newLength = fractalLength * length;
+        var newAngle = fractalAngle + angle;
+        draw(drawTable, spec, depth - 1, newX, newY, newLength, newAngle);
+    }
+}
+
+/**
+ * Draw the fractal to the draw table
+ * @param selector The draw table SVG DOM element
+ * @param spec The specification of the fractal
+ * Syntax: An array of [x,y,length,angle] values
+ * Each element describes a line: length length from (x,y) in angle radian
+ * The specification tells that from a line between (0;0) and (1;0) we must create these lines
+ */
+function drawFractal(selector, spec) {
+    var drawTable = document.querySelector(selector);
+
+    clearDrawTable(drawTable);
+    // TODO connect this function with the GUI
+    draw(drawTable, spec, 6, 20, 20, 729, 0);
+}
+
+/**
+ * Koch-curve description
+ * @see draw
+ */
+var koch_spec = [
     [0, 0, 1 / 3, 0],
     [1 / 3, 0, 1 / 3, Math.PI / 3],
     [1 / 2, Math.sqrt(3) / 6, 1 / 3, -Math.PI / 3],
     [2 / 3, 0, 1 / 3, 0]
 ];
-
-/**
- * Specify a fractal
- * TODO comment it right
- * @type {*[]}
- */
-var fractalSpec = koch_spec;
-
-// x,y,l,r a fenti negyes leirasaval egyezik
-/**
- * Draw the fractal recursively to the draw table
- * TODO comment it right
- * @param drawTable The draw table SVG DOM element
- * @param d
- * @param x
- * @param y
- * @param l
- * @param r
- */
-function draw(drawTable, d, x, y, l, r) {
-    var c = Math.cos(r);
-    var s = Math.sin(r);
-
-    if (d === 0) {
-        var x1 = x;
-        var y1 = y;
-        var x2 = c * l;
-        var y2 = s * l;
-        drawLine(drawTable, x1, y1, x2, y2);
-        return;
-    }
-
-    for (var i = 0; i < fractalSpec.length; i++) {
-        var sp = fractalSpec[i];
-        var x2 = sp[0], y2 = sp[1], l2 = sp[2], r2 = sp[3];
-        // (x;y) -> (x*c+y*s; -x*s+y*x)
-        var x3 = l * (x2 * c - y2 * s) + x;
-        var y3 = l * (y2 * c + x2 * s) + y;
-        var l3 = l2 * l;
-        var r3 = r2 + r;
-        draw(drawTable, d - 1, x3, y3, l3, r3);
-    }
-}
-
-/**
- * Drow the fractal to the draw table
- * @param selector The draw table SVG DOM element
- */
-function drawFractal(selector) {
-    drawTable = document.querySelector(selector);
-
-    clearDrawTable(drawTable);
-    // TODO connect this function with the GUI
-    // 5 szintu gorbe (20;20) ponttol, 729 hosszu vonal helyett
-    draw(drawTable, 5, 20, 20, 729, 0);
-}
-
 
